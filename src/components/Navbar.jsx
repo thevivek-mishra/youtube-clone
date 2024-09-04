f40535b284968662d9a1083bb60dae14e81ed645
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-09-04 21:53:11 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='flex justify-between px-6 py-2'>
-      <div className='border border-black'>1</div>
+      <div className='border border-black'>1
+        menubar
+        logo
+      </div>
       <div className='border border-black'>2</div>
       <div className='border border-black'>3</div>
     </div>
