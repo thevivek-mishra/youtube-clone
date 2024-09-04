1dc535eed14b0d71898c076f18ccfafb16e34694
vivek kumar mishra <vivekmishra4701@gmail.com>
2024-09-04 21:50:04 +0530

    project install

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex justify-between px-6 py-2'>
+      <div className='border border-black'>1</div>
+      <div className='border border-black'>2</div>
+      <div className='border border-black'>3</div>
+    </div>
+  )
+}
